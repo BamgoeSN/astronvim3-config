8b7347bd82a7e7a0c699983b3438e2020cdc71fc
BamgoeSN <60645387+BamgoeSN@users.noreply.github.com>
2023-04-13 17:59:28 +0900

    Initial commit

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,24 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+
+  {
+    "xiyaowong/nvim-transparent",
+    lazy = false,
+  },
+
+  {
+    "arcticicestudio/nord-vim",
+    config = function()
+      local colorScheme = vim.api.nvim_create_augroup("CustomColorScheme", { clear = true })
+      local commands = {
+        [[hi LineNr guifg=#7584A5 ctermfg=4 ctermbg=None]],
+        [[hi Comment guifg=#7584A5 ctermfg=4 ctermbg=None]],
+        [[hi Visual guibg=#4C566A ctermbg=4]],
+      }
+      for _, v in pairs(commands) do
+        vim.api.nvim_create_autocmd("ColorScheme", { group = colorScheme, command = v })
+      end
+    end,
+  },
 }
